26479c4b310e8aae59bcdfc757a24e0c77c1c35c
burakeven <burak.even@hotmail.com>
2021-07-26 16:42:25 +0300

    Correct Format

---
+++ Delegates/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Delegates
+{
+    public delegate void MyDelegate();//Void olan ve parametre almayan methodlara delegelik yapar.
+    public delegate void MyDelegate2(string text);//void olan ve string değer döndüren methodlara delegelik yapar.
+    public delegate int MyDelegate3(int x, int y);
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CustomManager customManager = new CustomManager();
+            //customManager.SendMessage();
+            //customManager.ShowAlert();
+            MyDelegate myDelegate = customManager.SendMessage;
+            myDelegate += customManager.ShowAlert;
+            myDelegate -= customManager.SendMessage;
+            myDelegate(); //delegeyi çağırmadan işlem gerçekleşmiyor.
+            Console.WriteLine("--------------");
+            MyDelegate2 myDelegate2 = customManager.SendMessage2;
+            myDelegate2 += customManager.ShowAlert2;
+            myDelegate2("hello");
+
+            newMaths math = new newMaths();
+            MyDelegate3 myDelegate3 = math.Topla;
+            var result=myDelegate3(2,5);
+            Console.WriteLine(result);
+            
+
+            Console.ReadLine();
+        }
+    }
+    public class CustomManager
+    {
+        public void SendMessage()
+        {
+            Console.WriteLine("Message has been sended.");
+        }
+        public void ShowAlert()
+        {
+            Console.WriteLine("Be Careful!");
+        }
+
+        public void SendMessage2(string message)
+        {
+            Console.WriteLine(message);
+        }
+        public void ShowAlert2(string alert)
+        {
+            Console.WriteLine(alert);
+        }
+    }
+    public class newMaths
+    {
+        public int Topla(int x, int y)
+        {
+            return x + y;
+        }
+        public int Carp(int x, int y)
+        {
+            return x * y;
+        }
+    }
+}
---
+++ Exceptions/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//RecordNotFoundException warning!
+namespace Exceptions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Find();
+            }
+            catch (RecordNotFoundException exception)
+            {
+
+                Console.WriteLine(exception.message);
+            }
+            catch(Exception exception)
+            {
+
+            }
+
+            //------------------------------------------------------
+            //Üsstekilerin aynısını bu yapıyor, methodlar ile don't repeat yourself kuralını uyguluyoruz.
+            //HandleException(() => {Find()});
+            HandleException(() =>
+            {
+                Find();
+            });
+            Console.ReadLine();
+
+        }
+        private static void HandleException(Action action)
+        {
+            try
+            {
+                action.Invoke();
+            }
+            catch (Exception exception)
+            {
+
+                Console.WriteLine(exception.Message); ;
+            }
+        }
+        private static void Find()
+        {
+            List<string> students = new List<string> { "Burak", "Buket", "Gaye" };
+            if (!students.Contains("Buket"))
+            {
+                throw new RecordNotFoundException("RecordNotFound");
+            }
+            else
+            {
+                Console.WriteLine("Record found.");
+            }
+        }
+    }
+}
